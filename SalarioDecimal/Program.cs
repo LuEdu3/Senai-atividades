b82852db52fed2b5e6ca270061b6cb8d04cc13c5
Luiz Eduardo <luizinformaticaeduardo@gmail.com>
2024-09-17 10:21:55 -0300

    adicionei 19 atividades

---
+++ SalarioDecimal/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Insira o salario: ");
+
+decimal result;
+if(decimal.TryParse(Console.ReadLine(),out decimal salario))
+{
+    if(salario<=3000)
+    {
+        result = salario*0.10m;
+        Console.Write($"O imposto sobre o salário é de R${result}");
+    }
+    else if(salario<=6000)
+    {
+        result = salario*0.15m;
+        Console.Write($"O imposto sobre o salário é de R${result}");
+    }
+    else
+    {
+        result = salario*0.20m;
+        Console.Write($"O imposto sobre o salário é de R${result}");
+    }
+}
+else
+{
+    Console.Write("Erro");
+} 
