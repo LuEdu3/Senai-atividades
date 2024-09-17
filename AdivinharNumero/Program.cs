4476a16315bdb813152f03e57768cfa8c73ed3d4
Luiz Eduardo <luizinformaticaeduardo@gmail.com>
2024-09-17 10:14:39 -0300

    adicionei 2 atividade

---
+++ AdivinharNumero/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.Numerics;
+
+int varvalor = 0;
+int vartotal = 0;
+
+Random randNum = new Random();
+int varnum = randNum.Next(100);
+
+while(varnum !=varvalor)
+{
+    Console.Write("Advinhe um número de 1 a 100 :");
+    
+    varvalor = int.Parse(Console.ReadLine());
+    
+    if(varvalor<varnum)
+    {
+        Console.WriteLine("Número Acima.");
+        vartotal++;
+    }
+    else if(varvalor>varnum)
+    {
+        Console.WriteLine("Número Abaixo.");
+        vartotal++;
+    }
+    else
+    {
+        Console.WriteLine("Certo.");
+    }
+}
+    Console.WriteLine($"A quantidade de tentativas foram {vartotal}.");
+    Console.WriteLine($"O número era {varnum}");
