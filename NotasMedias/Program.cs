fe7b160b8ff01f3d1594adf248b1dd32eb1ddbe5
Luiz Eduardo <luizinformaticaeduardo@gmail.com>
2024-09-17 10:21:26 -0300

    adicionei 14 atividades

---
+++ NotasMedias/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Insira a primeira nota: ");
+string nota = Console.ReadLine();
+float notas1 = float.Parse(nota);
+
+Console.Write("Insira a segunda nota: ");
+string nota2 = Console.ReadLine();
+float notas2 = float.Parse(nota2);
+
+Console.Write("Insira a terceira nota: ");
+string nota3 = Console.ReadLine();
+float notas3 = float.Parse(nota3);
+
+Console.Write("Insira a quarta nota: ");
+string nota4 = Console.ReadLine();
+float notas4 = float.Parse(nota4);
+
+float media = (notas1+notas2+notas3+notas4)/4;
+
+if(media>=60)
+{
+    Console.Write("\n---------Aluno Aprovado!---------");
+}
+else if(media>=40)
+{
+    Console.Write("\n---------Aluno de Recuperação!---------");
+}
+else
+{
+    Console.Write("\n---------Aluno Reprovado!--------");
+}
---
+++ NotasRepeticao/Program.cs
@@ -0,0 +1,14 @@
+﻿int notas = 0;
+int notasrep = 0;
+Console.Write("Quantas Notas deseja inserir: ");
+notasrep = int.Parse(Console.ReadLine());
+
+for(int i = 0; i<notasrep; i++)
+{
+    
+    Console.Write("Insira a nota: ");
+    notas = int.Parse(Console.ReadLine());
+}
+
+int media = notas/notasrep;
+Console.Write($"A média é {media}");
